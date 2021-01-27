1f2a87d16f1f73e34140fed43639d0778dd4f689
cvam77 <shivam98071@gmail.com>
2021-01-26 23:30:57 -0600

    front end almost ready

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,9 @@ import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import { Avatar, IconButton} from "@material-ui/core";
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from '../src/SidebarChat';
+
 
 function Sidebar() {
     return (
@@ -25,7 +28,16 @@ function Sidebar() {
             </div>
 
             <div className="sidebar_search">
+                  <div className="sidebar_searchContainer">
+                      <SearchOutlined/>
+                      <input placeholder="Search or start new chat" type="text"/>
+                  </div>
+            </div>
 
+            <div className="sidebar_chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
             </div>
         </div>
     )
